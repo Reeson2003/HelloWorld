6f3e0f9d347ab6032af7a7914f4607c4871a94e2
Pavel Gavrilov <Pavel Gavrilov>
2016-11-17 23:23:48 +0300

    Initial commit

---
+++ src/ru/reeson2003/MainProgramm.java
@@ -0,0 +1,10 @@
+package ru.reeson2003;
+
+/**
+ * Created by Toshiba on 17.11.2016.
+ */
+public class MainProgramm {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
